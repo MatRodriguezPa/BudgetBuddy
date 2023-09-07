ee9d8c256ff67704b682424db5b5df67e8927283
ESTUDIANTE <ESTUDIANTE@BE406S104C10ING.unal.loc>
2023-09-07 10:29:52 -0500

    1. HODSahdkasldjaslk

---
+++ BudgetBuddy/src/budgetbuddy/BudgetBuddy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package budgetbuddy;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class BudgetBuddy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
